ce0cb0330739fd77ebf755e353262c37c2691046
Michael <mikeystuart23@gmail.com>
2023-01-11 17:41:52 -0500

    nav bar login/logout

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,28 +1,87 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
 import { useState, useEffect } from "react";
-import useToken from "./Auth";
 
-function LoginModal(props) {
+function LoginModal() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleFormChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    handleClose();
+    e.preventDefault();
+    document.getElementById("form").reset();
+
+    const url = `${process.env.REACT_APP_RIPE_TOMATOES_API_HOST}/token`;
+    const form = new FormData();
+    form.append("username", formData.username);
+    form.append("password", formData.password);
+    console.log(formData.username);
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "include",
+      body: form,
+    });
+    if (response.ok) {
+      const data = await response.json();
+      window.location.reload();
+      return;
+    }
+  };
+
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Login</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Login</h4>
-        <p>Login</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <Button variant="secondary" onClick={handleShow}>
+        Login
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sign In</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form id="form">
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlInput1"
+              onChange={handleFormChange}
+            >
+              <Form.Label>Username</Form.Label>
+              <Form.Control name="username" type="text" autoFocus />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+              onChange={handleFormChange}
+            >
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" name="password" autoFocus />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="secondary" onClick={handleSubmit}>
+            Sign In
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
@@ -55,17 +114,23 @@ function Nav() {
   return (
     <>
       {token ? (
-        <Navbar bg="dark">
-          <button variant="primary" onClick={handleLogout}>
+        <Navbar bg="dark" variant="dark">
+          <Button pull-right variant="secondary" onClick={handleLogout}>
             Logout
-          </button>
-          <div>Hello {token.user.username}</div>
+          </Button>
+          <div
+            style={{
+              fontSize: "20px",
+              color: "white",
+              marginLeft: "auto",
+              marginRight: "20px",
+            }}
+          >
+            hi {token.user.username}
+          </div>
         </Navbar>
       ) : (
         <Navbar bg="dark">
-          <Button variant="primary" onClick={() => setLoginModalShow(true)}>
-            Login
-          </Button>
           <LoginModal
             show={loginModalShow}
             onHide={() => setLoginModalShow(false)}
