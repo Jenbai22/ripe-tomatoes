00a9dc9fba4fbd21d2c12694477eb68602c86f18
Leo Galvan <toro_negro25@yahoo.com>
2023-01-25 18:23:37 -0600

    yml10

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -4,16 +4,18 @@ import Home from "./pages/Home";
 import Nav from "./pages/nav/Nav";
 import Favorites from "./pages/Favorites";
 
-const domain = /https:\/\/[^/]+/;
-const basename = process.env.PUBLIC_URL.replace(domain, "");
+export default function App() {
+  const domain = /https:\/\/[^/]+/;
+  const basename = process.env.PUBLIC_URL.replace(domain, "");
 
-return (
-  <BrowserRouter basename={basename}>
-    <Nav />
-    <Routes>
-      <Route path="" element={<Home />} />
-      <Route path="/:imdb" element={<Detail />} />
-      <Route path="/favorites" element={<Favorites />} />
-    </Routes>
-  </BrowserRouter>
-);
+  return (
+    <BrowserRouter basename={basename}>
+      <Nav />
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/:imdb" element={<Detail />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
