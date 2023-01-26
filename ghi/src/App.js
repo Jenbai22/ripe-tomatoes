e697b81f408bab90a564255f342b89fbfb2bcb7e
Leo Galvan <toro_negro25@yahoo.com>
2023-01-25 18:06:48 -0600

    yml9

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,17 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import Nav from "./pages/nav/Nav";
-import Favorites from './pages/Favorites'
+import Favorites from "./pages/Favorites";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="/:imdb" element={<Detail />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const domain = /https:\/\/[^/]+/;
+const basename = process.env.PUBLIC_URL.replace(domain, "");
+
+return (
+  <BrowserRouter basename={basename}>
+    <Nav />
+    <Routes>
+      <Route path="" element={<Home />} />
+      <Route path="/:imdb" element={<Detail />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
+  </BrowserRouter>
+);
